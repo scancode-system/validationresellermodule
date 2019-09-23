86b1c36dab7ce6e4e52ed9d73bd2fb8ef0e10b15
Your Name <you@example.com>
2019-09-23 09:40:26 -0300

    .

--- Validator/ResellersValidator.php
+++ Validator/ResellersValidator.php
@@ -7,15 +7,14 @@ use Modules\Portal\Imports\ValidatorImport;
 class ResellersValidator extends ValidatorImport
 {	  
 
+	protected $required = ['login', 'senha'];
+
 	public function rule($data){
 		return  [
 			'id_representante' => 	'integer|min:1|unique_custom_values',
 			'nome' => 				'string|max:255',
-			'login' => 				'required|string|max:100|unique_custom_values',
-			'senha' => 				'required|string|max:255',
-			'cpf_cnpj' => 			'nullable|string|max:15|unique_custom_values',
-			'email' => 				'nullable|string|max:50|email',
-			'telefone' => 			'nullable|string|max:15',
+			'login' => 				'filled|string|max:100|unique_custom_values',
+			'senha' => 				'filled|string|max:255',
 			'admin' => 				'integer|between:0,1'
 		];
 	}
