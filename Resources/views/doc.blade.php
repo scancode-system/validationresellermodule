93d17f9e0b3579aafa941aa138a1b6f1f0b4e3e3
Your Name <you@example.com>
2019-10-07 01:30:55 -0300

    .

---
+++ Resources/views/doc.blade.php
@@ -0,0 +1,64 @@
+<div class="alert alert-info" role="alert">
+    <strong>Dados obrigatórios:</strong> login, senha
+</div>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+        <th scope="col">Campo</th>
+        <th scope="col">Tipo</th>
+        <th scope="col">Min</th>
+        <th scope="col">Max</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Número identificador do representante, código do representante">id_representante</span>
+        </td>
+        <td>Inteiro</td>
+        <td>1</td>
+        <td>N/A</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Nome do representante">nome</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Login do representante"><strong>login</strong></span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>100</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Senha de acesso do representante no aplicativo"><strong>senha</strong></span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Telefone de contato do representante">telefone</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>15</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Habilitar acesso administrado para o representante. 0 (não), 1 (sim)">admin</span>
+        </td>
+        <td>Inteiro</td>
+        <td>0</td>
+        <td>1</td>
+    </tr>
+
+</tbody>
+</table>
