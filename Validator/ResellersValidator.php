a3b9f1906f102c59fce70f8f2ba184ff6445bf02
Your Name <you@example.com>
2019-12-06 00:34:52 -0300

    .

--- Validator/ResellersValidator.php
+++ Validator/ResellersValidator.php
@@ -6,9 +6,22 @@ use Modules\Portal\Imports\ValidatorImport;
 use Modules\Portal\Rules\NotInCustomRule;
 
 class ResellersValidator extends ValidatorImport
-{	  
+{	
 
-	protected $required = ['login', 'senha'];
+	protected $required = ['id', 'name', 'email', 'goal', 'password'];
+
+	public function rule($data){
+		return  [
+			'id' => ['integer', 'min:1', new NotInCustomRule($this->chunkColumn('id_cliente', 0, $this->row_index-2), 'Duplicado')],
+			'name' => ['filled', 'string', 'max:255', new NotInCustomRule($this->chunkColumn('name', 0, $this->row_index-2), 'Duplicado')],
+			'email' => ['filled', 'string', 'max:255', new NotInCustomRule($this->chunkColumn('email', 0, $this->row_index-2), 'Duplicado')],
+			'goal' => 'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
+			'password' => 'filled|string|max:255'
+		];
+	}
+
+
+	/*protected $required = ['login', 'senha'];
 
 	public function rule($data){
 		return  [
@@ -22,5 +35,5 @@ class ResellersValidator extends ValidatorImport
 
 public function messages(){
 		return  [];
-	}
+	}*/
 }
