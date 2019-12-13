6fc65f12556cdf8a9d53aae03e599dc314c08f73
Your Name <you@example.com>
2019-12-13 18:33:50 -0300

    .

--- Config/config.php
+++ Config/config.php
@@ -1,32 +1,37 @@
 <?php
 
 return [
-    'name' => 'ValidationReseller', 
-    'fields' => ['id', 'name', 'email', 'goal', 'password'],
+	'name' => 'ValidationReseller', 
+	'fields' => ['id', 'name', 'email', 'goal', 'password'],
 	'sample' => [
 		[
 			'name' => 'id',
 			'observation' => 'Código do representante.',
+			'sample_1' => '1',
 			'filled' => false
 		], 
 		[
 			'name' => 'name',
 			'observation' => 'Nome do representante.',
+			'sample_1' => 'pedro',
 			'filled' => true
 		], 
 		[
 			'name' => 'email',
 			'observation' => 'Email do representante.',
+			'sample_1' => 'pedro@gmail.com.br',
 			'filled' => true
 		], 
 		[
 			'name' => 'goal',
 			'observation' => 'Meta de venda do representante.',
+			'sample_1' => '25000',
 			'filled' => false
 		], 
 		[
 			'name' => 'password',
 			'observation' => 'Senha utilizado no APP.',
+			'sample_1' => '12345678',
 			'filled' => true
 		]
 	]
