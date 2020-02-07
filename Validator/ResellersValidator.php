94c17646f3fde409363d835076910dcb64776693
Your Name <you@example.com>
2020-02-06 21:51:35 -0300

    .

--- Validator/ResellersValidator.php
+++ Validator/ResellersValidator.php
@@ -4,6 +4,7 @@ namespace Modules\ValidationReseller\Validator;
 
 use Modules\Portal\Imports\ValidatorImport;
 use Modules\Portal\Rules\NotInCustomRule;
+use Modules\Portal\Rules\NullRule;
 
 class ResellersValidator extends ValidatorImport
 {	
@@ -13,10 +14,10 @@ class ResellersValidator extends ValidatorImport
 	public function rule($data){
 		return  [
 			'id' => ['integer', 'min:1', new NotInCustomRule($this->chunkColumn('id', 0, $this->row_index-2), 'Duplicado')],
-			'name' => ['filled', 'string', 'max:255', new NotInCustomRule($this->chunkColumn('name', 0, $this->row_index-2), 'Duplicado')],
-			'email' => ['filled', 'string', 'max:255', new NotInCustomRule($this->chunkColumn('email', 0, $this->row_index-2), 'Duplicado')],
+			'name' => ['filled', 'string', 'max:191', new NotInCustomRule($this->chunkColumn('name', 0, $this->row_index-2), 'Duplicado')],
+			'email' => ['filled', 'string', 'max:191', new NotInCustomRule($this->chunkColumn('email', 0, $this->row_index-2), 'Duplicado')],
 			'goal' => 'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
-			'password' => 'filled|string|max:255'
+			'password' => 'filled|string|max:191'
 		];
 	}
 
