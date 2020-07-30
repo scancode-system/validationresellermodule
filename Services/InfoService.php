f8ee1c3a15227618ad279d74ce2aa614a91518b7
Your Name <you@example.com>
2020-07-30 16:07:15 -0300

    .

---
+++ Services/InfoService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\ValidationReseller\Services; 
+
+
+use Modules\Portal\Rules\NotInCustomRule;
+use Modules\Portal\Rules\NullRule;
+use Modules\Portal\Services\Validation\Data\InfoValidationService;
+use Modules\Portal\Services\Validation\Data\InfoValidationsService;
+
+
+class InfoService extends InfoValidationService
+{
+
+	public function rule($data, $index, $columns){
+		return  [
+			'id' => ['integer', 'min:1', new NotInCustomRule(InfoValidationService::chunkColumn($columns, 'id', 0, $index-2), 'Duplicado')],
+			'name' => ['filled', 'string', 'max:191', new NotInCustomRule(InfoValidationService::chunkColumn($columns, 'name', 0, $index-2), 'Duplicado')],
+			'email' => ['filled', 'string', 'max:191', new NotInCustomRule(InfoValidationService::chunkColumn($columns, 'email', 0, $index-2), 'Duplicado')],
+			'goal' => 'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
+			'password' => 'filled|string|max:191'
+		];
+	}
+
+	public function modifiers()
+	{
+		return [
+			['rule' => ['goal' => [new NullRule()]], 'filter' => 'setToZero']
+		];
+	}
+
+	public function columnsFormat()
+	{
+		return [
+			'name' => InfoValidationsService::STRING_FORMAT, 
+			'email' => InfoValidationsService::STRING_FORMAT,
+			'password' => InfoValidationsService::STRING_FORMAT
+		];
+	}
+
+}
